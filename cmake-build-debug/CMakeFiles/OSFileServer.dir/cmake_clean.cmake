36af09679e529dcf27d47384c4ec79aaec746d84
Jacob Beaton <beatonj00@gmail.com>
2020-12-01 18:02:57 -0500

    start

---
+++ cmake-build-debug/CMakeFiles/OSFileServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSFileServer.dir/main.c.o"
+  "OSFileServer"
+  "OSFileServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSFileServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
